90ecd719e59c07d03cead023fee83078e84eeeac
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-27 01:54:15 -0300

    chore: add pg and postgres setup

---
+++ src/db/postgres/helper.js
@@ -0,0 +1,22 @@
+import pg from 'pg'
+
+
+const { Pool } = pg
+
+ export const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB
+})
+
+export const PostgresHelper = {
+    query:  async (query, params) => {
+    const client = await pool.connect()
+    
+    const results = await client.query(query, params)
+    client.release()
+    return results.rows
+    }
+}
