8c924ef4f6fd9ae0fb9635d3e47b5b7e399e1d32
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-27 03:06:39 -0300

    test: use postman

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ import {PostgresHelper} from './src/db/postgres/helper.js'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', async (req, res) => {
     const results = await PostgresHelper.query('SELECT * FROM users;', [])
     res.send(JSON.stringify(results))
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.post('/users', async (req, res) => {
+    console.log(req.body)
+    console.log(req.headers)
+    res.status(201).send('User created successfully')
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
