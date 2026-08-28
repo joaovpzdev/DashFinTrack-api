152b36e5d39e8ae4e87ac955ec32cc80a54442d8
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-27 23:04:00 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,34 @@
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import bcrypt from 'bcrypt'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+import { EmailAlreadyInUse } from '../errors/email-already-in-use.js'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository = new PostgresGetUserByEmailRepository()
+
+            const userWithProvidedEmail = await postgresGetUserByEmailRepository.execute(updateUserParams.email,)
+
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUse('Email is already in use')
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(updateUserParams.password,10)
+
+            user.password = hashedPassword
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+
+        const updateUser = await postgresUpdateUserRepository.execute(userId, user)
+        return updateUser
+    }
+}
