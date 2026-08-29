72916889880e879423465ce42a7701f78748d9f3
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-29 17:09:48 -0300

    feat: add GetTransactionsByUserController

---
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,10 @@
+export class GetTransactionsByUserId {
+    async execute(params) {
+        try {
+            
+        } catch (error) {
+            console.error(error)
+            throw new Error('An error occurred while fetching transactions')
+        }
+    }
+}
