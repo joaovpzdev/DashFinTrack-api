db09a0871115dd4fed976cdfe69efe2fa85ea0aa
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-29 17:11:10 -0300

    feat: add get-transactions-by-user-id controller

--- src/controllers/transaction/get-transactions-by-user-id.js
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -1,10 +1,39 @@
+
+import { userNotFoundResponse } from "../helpers/user.js";
+import { ok, serverError } from "../helpers/http.js";
+import { UserNotFoundError } from "../helpers/user.js";
+import { checkIfIdIsValid, generateInvalidIdResponse, requiredFieldIsMissingResponse } from "../helpers/validation.js";
+
 export class GetTransactionsByUserId {
-    async execute(params) {
+    constructor(getTransactionsByUserIdUseCase) {
+        this.getTransactionsByUserIdUseCase = getTransactionsByUserIdUseCase
+    }
+    async execute(httpRequest) {
         try {
+            const userId = httpRequest.params.userId
+
+            if (!userId) {
+                return requiredFieldIsMissingResponse('userId')
+            }
+
+            const userIdIsValid = checkIfIdIsValid(userId)
+
+            if (!userIdIsValid) {
+                return generateInvalidIdResponse()
+            }
+
+            const transactions = await this.getTransactionsByUserIdUseCase.execute(userId)
+
+            return ok(transactions)
             
         } catch (error) {
-            console.error(error)
-            throw new Error('An error occurred while fetching transactions')
+            console.log(error)
+            
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+        }
+        
+            return serverError()
         }
     }
 }
