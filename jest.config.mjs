8580f5e79382ff5af9ea47dd7f14d1b59ff67147
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-31 20:14:52 -0300

    chore: add coverage tests script

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,9 @@
 
 /** @type {import('jest').Config} */
 const config = {
-  collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  collectCoverageFrom:["src/**/*.js"]
 };
 
 export default config;
