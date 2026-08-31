dbb90982b4748d714a7e896cc5484d96410290fa
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-31 15:53:27 -0300

    test: add dele transaction controller tests

---
+++ src/controllers/transaction/delete-transaction.test.js
@@ -0,0 +1,74 @@
+import { DeleteTransactionController } from './delete-transaction.js'
+import { faker } from '@faker-js/faker'
+
+describe('DeleteTransactionController', () => {
+  class DeleteTransactionUseCaseStub {
+    async execute() {
+      return {
+        user_id: faker.string.uuid(),
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        type: 'EXPENSE',
+        amount: Number(faker.finance.amount()),
+      }
+    }
+  }
+  const makeSut = () => {
+    const deleteTransactionUseCase = new DeleteTransactionUseCaseStub()
+    const sut = new DeleteTransactionController(deleteTransactionUseCase)
+    return { sut, deleteTransactionUseCase }
+  }
+
+  it('should return 200 when a transaction is successfully deleted', async () => {
+    const { sut } = makeSut()
+
+    const response = await sut.execute({
+      params: {
+        transactionId: faker.string.uuid(),
+      },
+    })
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  it('should return 400 when Id is invalid', async () => {
+    const { sut } = makeSut()
+
+    const response = await sut.execute({
+      params: {
+        transactionId: 'invalid_id',
+      },
+    })
+
+    expect(response.statusCode).toBe(400)
+  })
+
+  it('should return 404 when Transaction is not found', async () => {
+    const { sut, deleteTransactionUseCase } = makeSut()
+
+    jest
+      .spyOn(deleteTransactionUseCase, 'execute')
+      .mockResolvedValueOnce(null)
+
+    const response = await sut.execute({
+      params: {
+        transactionId: faker.string.uuid(),
+      },
+    })
+    expect(response.statusCode).toBe(404)
+  })
+
+  it('should return 500 when DeleteTransactionUseCase throws', async () => {
+    const { sut, deleteTransactionUseCase } = makeSut()
+
+    jest.spyOn(deleteTransactionUseCase, 'execute').mockRejectedValueOnce(new Error())
+
+    const response = await sut.execute({
+      params: {
+        transactionId: faker.string.uuid(),
+      },
+    })
+
+    expect(response.statusCode).toBe(500)
+  })
+})
